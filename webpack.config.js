e838a1c5017e80d739e70cd7086c34bc984775fa
lewebcode <levnikonenko@yandex.ru>
2023-04-24 01:08:37 +0300

    fixed styles and webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,52 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCss = require("mini-css-extract-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    mode: "development",
-    target: 'web',
-    devtool: 'source-map',
-    entry: path.resolve(__dirname, 'src', 'index.js'),
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        clean: true,
-        filename: "main.js",
-        assetModuleFilename: 'assets/[name][ext]',
+    mode: 'development',
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
     },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].[hash:8].js',
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'), // шаблон
+            filename: 'index.html', // название выходного файла
+        }),
+    ],
     module: {
         rules: [
             {
-                test: /\.html$/i,
-                loader: "html-loader",
-              },
-              {
-                test: /\.(c|sa|sc)ss$/i,
-                use: [
-                    MiniCss.loader, 
-                    "css-loader", 
-                    "sass-loader"
-                ],
-              },
+                test: /\.css$/,
+                use: ['style-loader', "css-loader"],
+            },
             {
-                test: /\.(jpeg|jpg|png|svg|gif)$/i,
-                type: 'asset/resource'
-            }, 
+                test: /\.html$/,
+                use: 'html-loader'
+            },
             {
-                test: /\.(ttf|woff2?)$/i,
-                type: "asset/resource",
-                generator: {
-                    filename: "fonts/[name].[ext]"
+                test: /\.(png|jpg|gif)$/,
+                type: 'asset/resource',
+                generator:  {
+                    filename: 'images/[name]-[hash][ext]',
+                }
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/resource',
+                generator:  {
+                    filename: 'fonts/[name]-[hash][ext]',
                 }
             }
-        ],
+        ]
     },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "src", "index.html")
-        }),
-        new MiniCss({
-            filename: "style.css",
-          })
-    ],
-
     devServer: {
         compress: false,
         open: true,
-        port: 5004,
-        // static: './public',
+        port: 3000,
         hot: true,
-    },
-};
+    }
+}

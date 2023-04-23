898a7cd0943e6387f5033a90471fdadfef1c8716
lewebcode <levnikonenko@yandex.ru>
2023-04-24 01:17:02 +0300

    add settings to optimization

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,46 +1,92 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'production',
-    entry: {
-        main: path.resolve(__dirname, './src/index.js'),
-    },
+    mode: "production",
+    target: 'browserslist',
+    entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
-        path: path.resolve(__dirname, './prod'),
-        filename: '[name].[hash:8].js',
+        path: path.resolve(__dirname, 'public'),
         clean: true,
+        filename: "[contenthash].js",
+        assetModuleFilename: 'assets/[hash][ext]',
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/index.html'), // шаблон
-            filename: 'index.html', // название выходного файла
-        }),
-    ],
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: ['style-loader', "css-loader"],
+                test: /\.html$/i,
+                loader: "html-loader",
             },
             {
-                test: /\.html$/,
-                use: 'html-loader'
+                test: /\.(c|sa|sc)ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    "postcss-preset-env",
+                                    // "postcss-deadcss",
+                                    // "at-rule-packer",
+                                    // "postcss-import"
+                                ],
+                            },
+                        },
+                    },
+                    "sass-loader"
+                ],
             },
             {
-                test: /\.(png|jpg|gif)$/,
-                type: 'asset/resource',
-                generator:  {
-                    filename: 'images/[name]-[hash][ext]',
-                }
+                test: /\.(jpeg|jpg|png|svg|gif)$/i,
+                use: [
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                          mozjpeg: {
+                            progressive: true,
+                          },
+                          // optipng.enabled: false will disable optipng
+                          optipng: {
+                            enabled: false,
+                          },
+                          pngquant: {
+                            quality: [0.65, 0.90],
+                            speed: 4
+                          },
+                          gifsicle: {
+                            interlaced: false,
+                          },
+                          // the webp option will enable WEBP
+                          webp: {
+                            quality: 75
+                          }
+                        }
+                      },
+                ],
+                type: 'asset/resource'
             },
             {
-                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-                type: 'asset/resource',
-                generator:  {
-                    filename: 'fonts/[name]-[hash][ext]',
+                test: /\.(ttf|woff2?)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: "fonts/[hash].[ext]"
                 }
             }
-        ]
-    }
-}
+        ],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src", "index.html")
+        })
+    ],
+
+    devServer: {
+        compress: false,
+        open: true,
+        hot: true,
+        port: 8081,
+    },
+};

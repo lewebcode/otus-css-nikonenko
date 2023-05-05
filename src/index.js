0225a04ebd72e72035be6bd8c150165d2f86674e
lewebcode <levnikonenko@yandex.ru>
2023-05-05 16:55:15 +0300

    add tailwind

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import "./css/animate.min.css";
-import "./css/fonts.css";
-import "./css/style.css";
 document.addEventListener("DOMContentLoaded", () => {
     function onEntry(entry) {
         entry.forEach(change => {
